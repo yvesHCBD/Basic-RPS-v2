9bf72b4422c14369776a3382a8e48b0337cd4590
yves <64351866+yveslapuz@users.noreply.github.com>
2023-01-09 01:42:05 -0800

    added round counter to game function to determine who won game

--- main.js
+++ main.js
@@ -22,25 +22,44 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "PAPER" && computerSelection === "ROCK") ||
     (playerSelection === "SCISSORS" && computerSelection === "PAPER")
   ) {
-    console.log(`PLAYER WINS! ${playerSelection} beats ${computerSelection}`);
+    // console.log(`PLAYER WINS! ${playerSelection} beats ${computerSelection}`);
+    return "PLAYER WINS";
   } else if (
     (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
     (computerSelection === "PAPER" && playerSelection === "ROCK") ||
     (computerSelection === "SCISSORS" && playerSelection === "PAPER")
   ) {
-    console.log(`COMPUTER WINS! ${computerSelection} beats ${playerSelection}`);
+    // console.log(`COMPUTER WINS! ${computerSelection} beats ${playerSelection}`);
+    return "COMPUTER WINS";
   } else if (playerSelection === computerSelection) {
-    console.log("IT'S A TIE!");
-  } else {
-    console.log("ERROR: INVALID CHOICE, CHOOSE ROCK, PAPER, OR SCISSORS");
+    // console.log("IT'S A TIE!");
+    return "TIE";
+    // } else {
+    //   // console.log("ERROR: INVALID CHOICE, CHOOSE ROCK, PAPER, OR SCISSORS");
+    // }
   }
 }
 
-function game() {
+function playGame() {
+  let playerCounter = 0;
+  let computerCounter = 0;
   for (let i = 1; i <= 5; i++) {
     console.log(`Game: ${i}`);
-    playRound(getPlayerChoice(), getComputerChoice());
+    if (playRound(getPlayerChoice(), getComputerChoice()) === "PLAYER WINS") {
+      playerCounter++;
+    } else {
+      computerCounter++;
+    }
+  }
+  if (playerCounter > computerCounter) {
+    console.log(
+      `PLAYER WON ${playerCounter} ROUNDS VS. COMPUTER'S ${computerCounter}`
+    );
+  } else {
+    console.log(
+      `COMPUTER WON ${computerCounter} ROUNDS VS. PLAYER'S ${playerCounter}`
+    );
   }
 }
 
-game();
+playGame();
