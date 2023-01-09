1b443eac1bf01a97138f63f0c2bb9567cbded6db
yves <64351866+yveslapuz@users.noreply.github.com>
2023-01-09 00:34:00 -0800

    finished basic logic for RPS game

---
+++ README.md
@@ -0,0 +1,2 @@
+#Extremely simple game of Rock-Paper-Scissors using javascript
+##Using the bare fundamentals of javascript's variables, loops, functions, and conditional logic to create a bare-bones rock paper scissors game.
---
+++ main.js
@@ -0,0 +1,43 @@
+console.log("Hello World");
+
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice === 0) {
+    return "ROCK";
+  } else if (computerChoice === 1) {
+    return "PAPER";
+  } else {
+    return "SCISSORS";
+  }
+}
+
+function getPlayerChoice() {
+  let userChoice = prompt("Choose: Rock, Paper, or Scissors");
+  return userChoice.toUpperCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+  ) {
+    console.log(`PLAYER WINS! ${playerSelection} beats ${computerSelection}`);
+  } else if (
+    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+    (computerSelection === "PAPER" && playerSelection === "ROCK") ||
+    (computerSelection === "SCISSORS" && playerSelection === "PAPER")
+  ) {
+    console.log(`COMPUTER WINS! ${computerSelection} beats ${playerSelection}`);
+  } else if (playerSelection === computerSelection) {
+    console.log("IT'S A TIE!");
+  } else {
+    console.log("ERROR: INVALID CHOICE, CHOOSE ROCK, PAPER, OR SCISSORS");
+  }
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
+playRound(getPlayerChoice(), getComputerChoice());
+playRound(getPlayerChoice(), getComputerChoice());
+playRound(getPlayerChoice(), getComputerChoice());
+playRound(getPlayerChoice(), getComputerChoice());
